c08a35de01790b91e7735e6e725b12d21dcfb7c1
Vaishnavi <vaishnavimh1965@gmail.com>
2025-08-22 16:36:30 +0530

    added the code for functionality

--- src/main/java/com/cognizant/App.java
+++ src/main/java/com/cognizant/App.java
@@ -1,7 +1,16 @@
 package com.cognizant;
 
+import com.cognizant.javaapp301.operation.ArmstrongNumber;
+
 public class App {
     public static void main(String[] args) {
     	
+    	        ArmstrongNumber arm = new ArmstrongNumber();
+
+    	        System.out.println(arm.defineArmstrongNumber());
+    	        System.out.println("Input: 153 → " + arm.verifyArmstrongNumber(153));
+    	        System.out.println("Input: 51 → " + arm.verifyArmstrongNumber(51));
+    	        System.out.println("Input: -10 → " + arm.verifyArmstrongNumber(-10));
+    	        System.out.println("Input: 0 → " + arm.verifyArmstrongNumber(0));
     }
 }
